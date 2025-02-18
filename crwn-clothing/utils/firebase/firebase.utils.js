d3c1571de68132201abdba80b795cd943174e1d6
YSW2 <jrgyber@gmail.com>
2025-02-18 18:03:35 +0900

    add gitignore firebase config

--- crwn-clothing/utils/firebase/firebase.utils.js
+++ crwn-clothing/utils/firebase/firebase.utils.js
@@ -5,15 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAoMJDpdqrV_OkbIRSXDOl2t_81VwP-E8g',
-  authDomain: 'crwn-clothing-db-6e248.firebaseapp.com',
-  projectId: 'crwn-clothing-db-6e248',
-  storageBucket: 'crwn-clothing-db-6e248.firebasestorage.app',
-  messagingSenderId: '505657851621',
-  appId: '1:505657851621:web:c1e99d20b7efe1e53e1e0c',
-};
+import { firebaseConfig } from '../../config/firebase/firebase.config';
 
 const firebaseApp = initializeApp(firebaseConfig);
 
