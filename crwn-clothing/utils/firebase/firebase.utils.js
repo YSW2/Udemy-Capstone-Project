01e165c8d1dce97ad92050d99e0a6e0639547aa3
YSW2 <jrgyber@gmail.com>
2025-02-19 01:46:02 +0900

    add firebase user documents

--- crwn-clothing/utils/firebase/firebase.utils.js
+++ crwn-clothing/utils/firebase/firebase.utils.js
@@ -5,6 +5,8 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 import { firebaseConfig } from '../../config/firebase/firebase.config';
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -17,3 +19,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
