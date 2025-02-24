28e0f0480cccd664d5d46db2081f9101085c7e24
YSW2 <jrgyber@gmail.com>
2025-02-24 16:22:23 +0900

    init context

--- crwn-clothing/src/routes/navigation/navigation.component.jsx
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CrwnLogo from '../../assets/crown.svg?react';
+import { UserContext } from '../../contexts/user.context';
+
+import { signOutUser } from '../../../utils/firebase/firebase.utils';
 
 import './navigation.style.scss';
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
+  const signOutHandler = async () => {};
+
   return (
     <Fragment>
       <div className="navigation">
@@ -16,9 +23,15 @@ const Navigation = () => {
           <Link className="nav-link" to={'/shop'}>
             SHOP
           </Link>
-          <Link className="nav-link" to={'/auth'}>
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="nav-link" to={'/auth'}>
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
